16629a244925280017f5221177bf21dd68d00fa1
Tony <waiting33118@gmail.com>
2020-04-23 09:42:46 +0800

    update

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const hostname = `localhost`
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
 	res.render('index')
 })
 
 app.post('/', (req, res) => {
+	console.log(req.body)
 	res.render('index')
 })
 
